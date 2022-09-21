c80d74130a10e83ea9100facc76d3b80c797ef46
Jairo Cruz <azaveliz@example.com>
2022-09-21 10:21:34 -0600

    Configure the test account

--- Program.cs
+++ Program.cs
@@ -36,9 +36,20 @@ builder.Services.AddControllers(config =>
 
 var app = builder.Build();
 
+
+
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+    // requires using Microsoft.Extensions.Configuration;
+    // Set password with the Secret Manager tool.
+    // dotnet user-secrets set SeedUserPW <pw>
+
+    var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");
 
     await SeedData.Initialize(services);
 }
